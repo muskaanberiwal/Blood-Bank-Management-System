8e94ac3bb7fc9c6f90ef61c92f48c265423e2c51
muskaanberiwal <64040643+muskaanberiwal@users.noreply.github.com>
2020-04-21 01:51:18 +0530

    Added project

---
+++ Muskaan's DBMS/src/muskaan/s/dbms/MuskaanSDBMS.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muskaan.s.dbms;
+
+/**
+ *
+ * @author rajiv
+ */
+public class MuskaanSDBMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
